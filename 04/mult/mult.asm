318110a6db0ceb03870791df6944df6f7e5fc448
Brenden Lowe <brendenlowe@gmail.com>
2019-02-05 21:28:03 -0800

    Completed the mult.asm script

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @R0
+    D=M
+    @valf
+    M=D
+    @R1
+    D=M
+    @counter
+    M=D
+    @R2
+    M=0
+(LOOP)
+    @counter
+    D=M
+    @END
+    D;JEQ
+    @valf
+    D=M
+    @R2
+    M=D+M
+    @counter
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
